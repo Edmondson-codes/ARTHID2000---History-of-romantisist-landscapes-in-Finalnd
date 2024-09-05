cf9915bfeafd59b05a22da0450c93c0f8f0a30a9
Jack <jack.eric.edmondson@gmail.com>
2024-09-05 11:22:41 +1000

    Q3 now gets data from relitive path so can run on any device

--- Q1/pca.py
+++ Q1/pca.py
@@ -2,6 +2,7 @@ from sklearn.datasets import fetch_lfw_people
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 import numpy as np
+# import torch as pca
 
 # Download the data, if not already on disk and load it as numpy arrays
 lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
--- Q3/unet_train.py
+++ Q3/unet_train.py
@@ -13,30 +13,31 @@ from unet_model import UNet
 
 
 
-class CustomImageDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(annotations_file)
-        self.img_dir = img_dir
-        self.transform = transform
-        self.target_transform = target_transform
-
-    def __len__(self):
-        return len(self.img_labels)
-
-    def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
-        label = self.img_labels.iloc[idx, 1]
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-        return image, label
+# class CustomImageDataset(Dataset):
+#     def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+#         self.img_labels = pd.read_csv(annotations_file)
+#         self.img_dir = img_dir
+#         self.transform = transform
+#         self.target_transform = target_transform
+#
+#     def __len__(self):
+#         return len(self.img_labels)
+#
+#     def __getitem__(self, idx):
+#         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+#         image = read_image(img_path)
+#         label = self.img_labels.iloc[idx, 1]
+#         if self.transform:
+#             image = self.transform(image)
+#         if self.target_transform:
+#             label = self.target_transform(label)
+#         return image, label
 
 # image_dataset = CustomImageDataset()
 
+print()
 
-data_dir = "/home/pickle/Documents/Projects/COMP3710/Tut2/Q3/keras_png_slices_data"
+data_dir = "./keras_png_slices_data" # "/home/pickle/Documents/Projects/COMP3710/Tut2/Q3/keras_png_slices_data"
 
 train_transforms = transforms.Compose([transforms.RandomRotation(30),
                                        transforms.RandomResizedCrop(224),
