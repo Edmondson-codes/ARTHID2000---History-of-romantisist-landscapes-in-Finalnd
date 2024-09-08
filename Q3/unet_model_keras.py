3b583c4f6c30c1db5548d9ad1db428b6de17843a
Jack <jack.eric.edmondson@gmail.com>
2024-09-08 14:18:49 +1000

    Polished Q3. Need to implement img display still.

--- Q3/unet_model_keras.py
+++ Q3/unet_model_keras.py
@@ -1,6 +1,7 @@
 from keras import Sequential
 from keras.src.layers import Conv2D, BatchNormalization, Dropout, Dense, Flatten, MaxPooling2D, preprocessing, Input
 from keras.src.legacy.preprocessing.image import ImageDataGenerator
+from keras.src.metrics.metrics_utils import confusion_matrix
 from keras.src.utils import to_categorical
 from keras_preprocessing.image import load_img, img_to_array
 import numpy as np
@@ -9,16 +10,24 @@ import keras
 import tensorflow as tf
 from tensorflow.python.keras import backend as K
 import matplotlib.pyplot as plt
+from keras import layers, models
+from keras._tf_keras.keras.models import load_model
 
 # K.tensorflow_backend._get_available_gpus()
 # sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
 #
 # keras.backend.set_session(sess)
 
-sess = tf.compat.v1.Session()
-K.set_session(sess)
+# sess = tf.compat.v1.Session()
+# K.set_session(sess)
+
+# ============== Vars ==============
 
 FULL_SIZE_IMG = 1  # set to 2 to use full size image
+INPUT_SHAPE = (32, 32, 3)
+num_classes = 4  # numb of classes in segmentation
+
+# ============== Get Data ==============
 
 def load_and_preprocess_image(image_path, target_size):
     """Load an image, resize it, and normalize it."""
@@ -31,10 +40,8 @@ def load_data(image_folder, mask_folder, target_size):
     """Load and preprocess images and masks."""
     # sorted takes in an array.
     # the for loop creates an array with all the png names in a folder.
-    # https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
     image_filenames = sorted([f for f in os.listdir(image_folder) if f.endswith('.png')])
     mask_filenames = sorted([f for f in os.listdir(mask_folder) if f.endswith('.png')])
-    # print([f for f in os.listdir(mask_folder) if f.endswith('.png')])
 
     images = []
     masks = []
@@ -65,47 +72,12 @@ test_images, test_masks = load_data('keras_png_slices_data/test/keras_png_slices
 train_masks = train_masks.astype(np.float32)
 test_masks = test_masks.astype(np.float32)
 
-num_classes = 4  # numb of classes in segmentation
-
 # Convert masks to categorical one-hot encodings
 train_masks = to_categorical(train_masks, num_classes=num_classes)
 test_masks = to_categorical(test_masks, num_classes=num_classes)
 
 
-
-# TODO: see how lecturer does this in monday or **Wend** presentation & Improve >:)
-
-INPUT_SHAPE = (32, 32, 3)
-
-# def model():
-#     inputs = Input(INPUT_SHAPE)
-#     e1 = Conv2D(filters=16, kernel_size=(3, 3), input_shape=INPUT_SHAPE, activation='relu', padding='same')(inputs)
-#     e1 = Conv2D(filters=16, kernel_size=(3, 3), input_shape=INPUT_SHAPE, activation='relu', padding='same')(e1)
-#     p1 = MaxPooling2D(pool_size=(2, 2))(e1)
-
-
-# model = Sequential()
-# model.add(Conv2D(filters=16, kernel_size=(3, 3), input_shape=INPUT_SHAPE, activation='relu', padding='same'))
-# model.add(BatchNormalization())
-# model.add(Conv2D(filters=16, kernel_size=(3, 3), input_shape=INPUT_SHAPE, activation='relu', padding='same'))
-# model.add(BatchNormalization())
-#
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Dropout(0.25))
-#
-# model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=INPUT_SHAPE, activation='relu', padding='same'))
-# model.add(BatchNormalization())
-# model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=INPUT_SHAPE, activation='relu', padding='same'))
-# model.add(BatchNormalization())
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-#
-# model.add(Flatten())
-# model.add(Dense(32, activation='relu'))
-# model.add(Dense(10, activation='softmax'))
-#
-# model.summary()
-
-from keras import layers, models
+# ============== Model ==============
 
 
 def unet_model(input_size=(128*FULL_SIZE_IMG, 128*FULL_SIZE_IMG, 1)):
@@ -127,10 +99,6 @@ def unet_model(input_size=(128*FULL_SIZE_IMG, 128*FULL_SIZE_IMG, 1)):
     conv3 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(conv3)
 
     outputs = layers.Conv2D(4, (1, 1), activation='sigmoid')(conv3)
-    #
-    # flat = layers.Flatten()(conv3)
-    # relu = layers.Dense(64, activation='relu')(flat)
-    # outputs = layers.Dense(10, activation='softmax')(relu)
 
     model = models.Model(inputs=[inputs], outputs=[outputs])
     model.summary()
@@ -143,24 +111,31 @@ model = unet_model()
 
 
 
-# Train Model
-# model.compile(loss='categorical_crossentropy',
-#                optimizer='adam',
-#                metrics=['accuracy']
-#               )
+# ============== Train or Load Model ==============
 
-history = model.fit(
-    train_images,
-    train_masks,
-    validation_split=0.1,
-    epochs=1,
-    batch_size=16
-)
+if os.path.isfile('models/unet_model_keras_128_res.h5') is True:
+    print('Training Model')
+    # Train
+    history = model.fit(
+        train_images,
+        train_masks,
+        validation_split=0.1,
+        epochs=1,
+        batch_size=16
+    )
 
-# Evaluate model
+    model.save('models/unet_model_keras_128_res.h5')
+
+else:
+    print("Loading Model")
+    model = load_model('models/unet_model_keras_128_res.h5')
+
+
+# ============== Test Model ==============
+
+
 loss, accuracy = model.evaluate(test_images, test_masks)
 
-# Make predictions
 predictions = model.predict(test_images)
 
 print(f"Loss: {loss}")
