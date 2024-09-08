5417a02e1b35b7abc92d074eaca8545771ffd7ec
Jack <jackery.outlook@gmail.com>
2024-09-08 14:00:37 +1000

    Implemented Q2.1

--- Q2/faces_cnn.py
+++ Q2/faces_cnn.py
@@ -1,4 +1,5 @@
 from keras.src.legacy.preprocessing.image import ImageDataGenerator
+from keras.src.utils import to_categorical
 from sklearn.datasets import fetch_lfw_people
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
@@ -15,6 +16,7 @@ from keras.src.optimizers import Adam
 # ================= Vars =================
 
 BATCH_SIZE = 10
+NUM_CLASSES = 7
 
 # ================= Get data =================
 lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
@@ -24,48 +26,57 @@ n_samples, h, w = lfw_people.images.shape    # TODO: h & w here, reshape using t
 X = lfw_people.data
 n_features = X.shape[1]
 
-X.reshape(n_samples, h, w, 3)
+X = X.reshape(n_samples, h, w, 1)
 
-print(f"Shape: {X.shape}")
+print(f"Shape of X: {X.shape}")                     # Shape of X: (1288, 50, 37, 1)
+print(f"Shape of imgs: {lfw_people.images.shape}")  # Shape of imgs: (1288, 50, 37)
 
 y = lfw_people.target
+# y = to_categorical(y, num_classes=NUM_CLASSES)
 target_names = lfw_people.target_names
+
+print(f"Shape of y: {y.shape}")   # Shape of y: (1288,)
 
 n_classes = target_names.shape[0]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
-# TODO: reformat X & y so that they have shape (n_samples, height, width, channels)
-# currently: n_samples = 1288,
+print(f"labels Head: {y_train}")
 
-data_generator = ImageDataGenerator(horizontal_flip=True)
+data_generator = ImageDataGenerator()  # rescale=1./255
 
 # error here
-train_generator = data_generator.flow(X_train, y_train, BATCH_SIZE).shuffle()  # Added shuffle
+train_generator = data_generator.flow(X_train, y_train, BATCH_SIZE)
+
+print(train_generator)
+
 
 # ================= Model =================
+
 model = Sequential([
     Conv2D(filters=32, kernel_size=(3,3), activation='relu', padding='same'),
     Conv2D(filters=32, kernel_size=(3,3), activation='relu', padding='same'),
     Dense(units=32, activation='relu'),
-    Dense(units=7, activation='softmax')
+    Flatten(),
+    Dense(units=n_classes, activation='softmax')
 ])
 
+model.compile(
+    optimizer=Adam(learning_rate=0.0001),
+	loss='sparse_categorical_crossentropy',   # this makes the output a one-hot encoding
+	metrics=['accuracy'],
+)
+
+print(model.summary())
 
 # ================= Training =================
 
-model.compile(
-    optimizer=Adam(learning_rate=0.0001), # learning rate
-	loss='categorical_crossentropy',
-	metrics=['accuracy','loss']
-)
-
 model.fit(
 	train_generator,
-	validation_split=0.05,
 	epochs=10,
 	verbose=2
 )
+
 
 # ================= Results =================
 
@@ -73,4 +84,4 @@ predictions = model.predict(x=X_test, batch_size=BATCH_SIZE, verbose=0)
 rounded_predictions = np.argmax(predictions, axis=-1)
 correct = rounded_predictions==y_test
 
-print(f'Classification report: {classification_report(y_test, rounded_predictions)}')
+print(f'Classification report: \n{classification_report(y_test, rounded_predictions)}')
