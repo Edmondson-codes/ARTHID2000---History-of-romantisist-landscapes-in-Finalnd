f21b05197f7b986a6bc2366d455ca160662052ea
Jack <jackery.outlook@gmail.com>
2024-09-09 12:28:09 +1000

    implmented Q1

--- Q1/pca.py
+++ Q1/pca.py
@@ -1,8 +1,9 @@
+import keras.src.ops
 from sklearn.datasets import fetch_lfw_people
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 import numpy as np
-# import torch as pca
+import torch
 
 # Download the data, if not already on disk and load it as numpy arrays
 lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
@@ -32,19 +33,26 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 # dataset): unsupervised feature extraction / dimensionality reduction
 n_components = 150
 
+X_train = torch.from_numpy(X_train)
+X_test = torch.from_numpy(X_test)
+y_train = torch.from_numpy(y_train)
+y_test = torch.from_numpy(y_test)
+
 
 # Center data
-mean = np.mean(X_train, axis=0)
+mean = torch.mean(X_train)
 X_train -= mean
 X_test -= mean
+print(f"Shape of X_train: {X_train.shape}")
 #Eigen-decomposition
-U, S, V = np.linalg.svd(X_train, full_matrices=False)
+U, S, V = torch.linalg.svd(X_train)   # uses torch now
+# U, S, V =keras.src.ops.eig(X_train)
 components = V[:n_components]
 eigenfaces = components.reshape((n_components, h, w))
 #project into PCA subspace
-X_transformed = np.dot(X_train, components.T)
+X_transformed = torch.mm(X_train, components.T)
 print(X_transformed.shape)
-X_test_transformed = np.dot(X_test, components.T)
+X_test_transformed = torch.mm(X_test, components.T)
 print(X_test_transformed.shape)
 
 
